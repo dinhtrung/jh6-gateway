0d5c303b1d0eb0a21f3bb6793789943af0035c4c
Trung Nguyen <nguyendinhtrung141@gmail.com>
2020-02-24 22:13:59 +0700

    Add themer page

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { SharedModule } from 'app/shared/shared.module';
-import { HOME_ROUTE } from './home.route';
+import { HOME_ROUTES } from './home.route';
 import { HomeComponent } from './home.component';
+// + rendering markdown pages
+import { MarkdownModule } from 'ngx-markdown';
+import { DocsComponent } from './docs/docs.component';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent]
+  imports: [SharedModule, MarkdownModule.forRoot(), RouterModule.forChild(HOME_ROUTES)],
+  declarations: [HomeComponent, DocsComponent]
 })
 export class GatewayHomeModule {}
